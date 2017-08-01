89a994f3a6cb1c2417dbb062e7032c97039097e4
Robert Nad <testolog5@gmail.com>
2017-08-01 19:11:10 +0300

    Update BaseLogic.scala
    done steps 1, 4,2

--- src/main/scala/net/robert/scala/spark/BaseLogic.scala
+++ src/main/scala/net/robert/scala/spark/BaseLogic.scala
@@ -1,51 +1,55 @@
 package net.robert.scala.spark
 
-import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.{DataFrame, Row}
 import org.apache.spark.sql.functions._
-import org.apache.spark.sql.types.{ArrayType, DataType, StringType}
+import org.apache.spark.sql.types._
 
 class BaseLogic {
-
   def info(dataFrame: DataFrame): DataFrame = {
-//    var base = dataFrame.columns.foldLeft(dataFrame)((acc, name) => {
-//      countByName(acc, name)
-//    })
-//
-//    base.show()
-//
-//    dataFrame
-    countByName(dataFrame, "name")
+    val schema = dataFrame.schema.fieldNames
+    val base = dataFrame.flatMap(x => {
+      x.getValuesMap[String](schema).map(index => {
+        Row(index._1, index._2)
+      })
+    }).filter(x => {
+      if (x.getString(1) != null) {
+        !x.getString(1).isEmpty && !x.getString(1).equals(" ")
+      } else {
+        true
+      }
+    })
+    val structure = new StructType(Array(
+      StructField("key", StringType, nullable = false),
+      StructField("value", StringType, nullable = true)
+    ))
+    var changes = dataFrame.sqlContext.createDataFrame(base, structure)
+
+    countByName(changes)
   }
 
-  def countByName(dataFrame: DataFrame, name: String): DataFrame = {
+  def countByName(dataFrame: DataFrame): DataFrame = {
     val collection_list = new CollectionList[ArrayType](ArrayType(StringType))
     val base = dataFrame.repartition(3)
-      .select(lit(name).alias("key"), col(name).alias("value"))
       .groupBy(col("value"))
-      .agg(countDistinct(col("value")).alias("unique_value")).repartition(3)
-      .select(array(col("value"), col("unique_value")).alias("values"), lit(name).alias("key"))
-    val uniqueCount = dataFrame.repartition(3).select(lit(name).alias("key"), countDistinct(col(name)).alias("unique_count"))
-    base.join(uniqueCount).repartition(3)
-      .select(base.apply("key"), col("values"), col("unique_count"))
+      .agg(count(col("value")).alias("value_count"))
       .repartition(3)
-      .groupBy("key", "unique_count")
+      .join(dataFrame.repartition(3)
+        .groupBy(col("key"), col("value"))
+        .agg(countDistinct("value").as("stg_count"))
+        .repartition(3), "value")
+    base.join(base.repartition(3)
+      .groupBy(col("key"))
+      .agg(sum("stg_count").as("unique_value")),
+      "key")
+      .repartition(3)
+      .select(col("key"), col("unique_value"), array(col("value"), col("value_count")).as("values"))
+      .groupBy(col("key"), col("unique_value"))
       .agg(collection_list(col("values")).alias("values"))
-      .select(col("key").alias("column"),
-        col("unique_count").alias("unique_values"),
-        col("values").alias("values")
-      )
-
-
-    //      .select(
-    //        lit(name).alias("column"),
-    //        countDistinct(col(name)).alias("uniqueValue")
-    //      )
   }
 
-  def transfer(dataFrame: DataFrame): DataFrame = {
-    dataFrame.na.drop()
-  }
   def validate(dataFrame: DataFrame, schema: String): DataFrame= {
-    dataFrame
+    dataFrame.columns.foldLeft(dataFrame)((df, name) =>{
+      dataFrame.withColumn(name)
+    })
   }
 }
